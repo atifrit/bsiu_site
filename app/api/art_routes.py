a00af1cf4665eee622e5916abafb4b5460623b9a
atifrit <adamtifrit@gmail.com>
2024-02-26 13:40:25 -0800

    added backend routes for posting and deleting art

--- app/api/art_routes.py
+++ app/api/art_routes.py
@@ -1,7 +1,14 @@
 from flask import Blueprint, jsonify, session, request
 from app.models import Art, db
 from flask_login import current_user, login_user, logout_user, login_required
+from app.forms import ArtForm
 
+def validation_errors_to_error_messages(validation_errors):
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f'{field} : {error}')
+    return errorMessages
 
 art_routes = Blueprint('art', __name__)
 
@@ -31,4 +38,32 @@ def get_all_art():
         return jsonify([])
 
 @art_routes.route('/', methods=['POST'])
+@login_required
 def post_art():
+    artForm=ArtForm()
+    artForm['csrf_token'].data = request.cookies['csrf_token']
+
+    if artForm.validate_on_submit():
+        artCount = Art.query.all().count()
+
+        orderNum = (artCount + 1)*1024
+
+        new_art = Art(name=artForm.data['name'], image=artForm.data['image'], caption=artForm.data['caption'], year=artForm.data['year'], category=artForm.data['category'], order=orderNum, notes=artForm.data['notes'])
+        db.session.add(new_art)
+        db.session.commit()
+        return ({'message': 'Art Added Successfully'})
+
+    return {'errors': validation_errors_to_error_messages(artForm.errors)}, 401
+
+@art_routes.route('/<int:art_id>', methods=['DELETE'])
+@login_required
+def delete_art(art_id):
+
+    art = Art.query.get(art_id)
+
+    if not art:
+        return jsonify({'error': 'Art not found'}), 404
+
+    db.session.delete(art)
+    db.session.commit()
+    return jsonify({'message': 'successfully deleted'})
